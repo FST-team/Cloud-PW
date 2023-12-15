bd6028e8b9a2591911c50eed6b19ab6c92113c42
graziaFerrara <grazia.ferrara2000@gmail.com>
2023-12-16 00:36:45 +0100

    found first possible configuration for 5/9

---
+++ availability/sfc_rbd.m
@@ -0,0 +1,13 @@
+format long
+
+avP = 0.998259001;
+avS = avP;
+avIH = 0.997671992;
+
+% first possible configuration
+
+redP = 3;
+redS = 2;
+redIH = 2;
+
+parallel = (1-(1-avP)^redP)*(1-(1-avS)^redS)*(1-(1-avIH)^redIH)
