b60a9b4c81b15d34ca7daaff2adcbca0cee01c75
Felice Scala <scala.felice@hotmail.it>
2023-12-21 13:20:01 +0100

    Divided code in functions for open multi-class network

--- performances/multi-class-bcmp/Esercizio_231221.m
+++ performances/multi-class-bcmp/Esercizio_231221.m
@@ -64,36 +64,7 @@ T_tot_limit = 200*10^-3;
 
 %-------------------------------------------------------------------------------
 %% Recupero della configurazione che rispetta E[T] <= 200 per entrambe le classi
-T_tot_vector = [+Inf +Inf]';
-while any(T_tot_vector > [T_tot_limit T_tot_limit]')
-  try
-    % Calcolo del vettore di visite per la rete multi-class open
-    V = qnomvisits(P, lambda_ext_matrix);
+m_vector = qnomHelpFunctions.qnomGetWorkingConfiguration(P, lambda_ext_matrix, S, [T_tot_limit; T_tot_limit], m_vector);
 
-    % Analisi della rete
-    [U,R,Q,X] = qnom(lambda_ext_vector, S, V, m_vector);
-
-    % Calcolo del tempo medio per ogni classe
-    T_tot_vector = T_tot_func(Q, lambda_ext_vector');
-
-    if any(T_tot_vector > [T_tot_limit T_tot_limit]')
-      [maxU_class1, maxU_class1_index] = max(U(1,:));
-      [maxU_class2, maxU_class2_index] = max(U(2,:));
-
-      if maxU_class1_index == maxU_class2_index
-        m_vector(maxU_class1_index) = m_vector(maxU_class1_index) + 1;
-      else
-        m_vector(maxU_class1_index) = m_vector(maxU_class1_index) + 1;
-        m_vector(maxU_class2_index) = m_vector(maxU_class2_index) + 1;
-      endif
-    endif
-  catch err
-    lastNumber = sscanf(err.message, 'Processing capacity exceeded at center %d');
-    m_vector(lastNumber) = m_vector(lastNumber) + 1;
-    T_tot_vector = [+Inf +Inf]';
-  end_try_catch
-  display('Nuovo vettore M:');
-  m_vector
-  display('Nuovo vettore dei tempi totali medi:');
-  T_tot_vector
-end
+% Plot del tempo medio per classe con la configurazione trovata
+qnomHelpFunctions.qnomPlotTtotSurface(P,S,m_vector,T_tot_limit);
